5a8f690c80d8b96167b96488abde2272d2fdedc4
easXin <codingericwang@gmail.com>
2020-03-03 11:50:47 -0800

    modify cucumberoptions

--- eclipse-workspace/calculator/src/test/java/cucumberRunner/RunCalculatorTest.java
+++ eclipse-workspace/calculator/src/test/java/cucumberRunner/RunCalculatorTest.java
@@ -10,7 +10,7 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 	features = "src/test/resources/features/", 
-    glue = "MyStepDefinitions",
+    glue = "stepDefinitions",
     tags = "@test")
 
 public class RunCalculatorTest {
